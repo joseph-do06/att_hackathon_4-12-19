3d73bcc399906330574f7f4fb0ec161577397462
saemi.lee.ram <saemi.lee.ram@gmail.com>
2019-04-12 21:52:10 -0700

    services got simpler

--- hack-messenger/src/service/PersonInsightService.jsx
+++ hack-messenger/src/service/PersonInsightService.jsx
@@ -1,9 +1,8 @@
 import Axios from "axios";
-import * as helpers from "./serviceHelper";
 
 const BaseURL =
   "https://gateway.watsonplatform.net/personality-insights/api/v3/profile?version=2017-10-13";
-const insightPost = payload => {
+const insightPost = (payload, onSuccess, onError) => {
   const config = {
     method: "POST",
     url: BaseURL,
@@ -12,73 +11,9 @@ const insightPost = payload => {
     crossdomain: true,
     headers: { "Content-Type": "application/json" }
   };
-
   return Axios(config)
-    .then(helpers.onGlobalSuccess)
-    .catch(helpers.onGlobalError);
+    .then(onSuccess)
+    .catch(onError);
 };
 
-const insightGetAll = () => {
-  const config = {
-    method: "GET",
-    url: BaseURL,
-    withCredentials: true,
-    crossdomain: true,
-    headers: { "Content-Type": "application/json" }
-  };
-
-  return Axios(config)
-    .then(helpers.onGlobalSuccess)
-    .catch(helpers.onGlobalError);
-};
-
-const insightGetById = id => {
-  const config = {
-    method: "GET",
-    url: BaseURL + "/" + id,
-    withCredentials: true,
-    crossdomain: true,
-    headers: { "Content-Type": "application/json" }
-  };
-
-  return Axios(config)
-    .then(helpers.onGlobalSuccess)
-    .catch(helpers.onGlobalError);
-};
-
-const insightUpdate = payload => {
-  const config = {
-    method: "PUT",
-    url: BaseURL + "/" + payload.id,
-    data: payload,
-    withCredentials: true,
-    crossdomain: true,
-    headers: { "Content-Type": "application/json" }
-  };
-
-  return Axios(config)
-    .then(helpers.onGlobalSuccess)
-    .catch(helpers.onGlobalError);
-};
-
-const insightDelete = id => {
-  const config = {
-    method: "DELETE",
-    url: BaseURL + "/" + id,
-    withCredentials: true,
-    crossdomain: true,
-    headers: { "Content-Type": "application/json" }
-  };
-
-  return Axios(config)
-    .then(helpers.onGlobalSuccess)
-    .catch(helpers.onGlobalError);
-};
-
-export {
-  insightPost,
-  insightGetAll,
-  insightGetById,
-  insightUpdate,
-  insightDelete
-};
+export { insightPost };
--- hack-messenger/src/service/ToneAnalyzerService.jsx
+++ hack-messenger/src/service/ToneAnalyzerService.jsx
@@ -1,9 +1,8 @@
 import Axios from "axios";
-import * as helpers from "./serviceHelper";
 
 const BaseURL =
-  "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21";
-const analyzerPost = payload => {
+  "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21&sentences=false";
+const analyzerPost = (payload, onSuccess, onError) => {
   const config = {
     method: "POST",
     url: BaseURL,
@@ -14,71 +13,8 @@ const analyzerPost = payload => {
   };
 
   return Axios(config)
-    .then(helpers.onGlobalSuccess)
-    .catch(helpers.onGlobalError);
+    .then(onSuccess)
+    .catch(onError);
 };
 
-const analyzerGetAll = () => {
-  const config = {
-    method: "GET",
-    url: BaseURL,
-    withCredentials: true,
-    crossdomain: true,
-    headers: { "Content-Type": "application/json" }
-  };
-
-  return Axios(config)
-    .then(helpers.onGlobalSuccess)
-    .catch(helpers.onGlobalError);
-};
-
-const analyzerGetById = id => {
-  const config = {
-    method: "GET",
-    url: BaseURL + "/" + id,
-    withCredentials: true,
-    crossdomain: true,
-    headers: { "Content-Type": "application/json" }
-  };
-
-  return Axios(config)
-    .then(helpers.onGlobalSuccess)
-    .catch(helpers.onGlobalError);
-};
-
-const analyzerUpdate = payload => {
-  const config = {
-    method: "PUT",
-    url: BaseURL + "/" + payload.id,
-    data: payload,
-    withCredentials: true,
-    crossdomain: true,
-    headers: { "Content-Type": "application/json" }
-  };
-
-  return Axios(config)
-    .then(helpers.onGlobalSuccess)
-    .catch(helpers.onGlobalError);
-};
-
-const analyzerDelete = id => {
-  const config = {
-    method: "DELETE",
-    url: BaseURL + "/" + id,
-    withCredentials: true,
-    crossdomain: true,
-    headers: { "Content-Type": "application/json" }
-  };
-
-  return Axios(config)
-    .then(helpers.onGlobalSuccess)
-    .catch(helpers.onGlobalError);
-};
-
-export {
-  analyzerPost,
-  analyzerGetAll,
-  analyzerGetById,
-  analyzerUpdate,
-  analyzerDelete
-};
+export { analyzerPost };
