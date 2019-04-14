f4e78f2f547465e490e57cab0ff373627631fc5a
Jason Gillham <gillham01@gmail.com>
2019-04-14 04:07:22 -0700

    dashboard layout

---
+++ hack-messenger/src/service/PersonalityInsightsService.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+class PersonalityInsightsService {
+  static InsightsPost(data, onSuccess, onError) {
+    const url =
+      "https://gateway.watsonplatform.net/personality-insights/api/v3/profile?version=2017-10-13";
+    const config = {
+      method: "POST",
+      data: `{"text": "${data}"}`,
+      headers: {
+        "Content-Type": "text/plain;charset=utf-8",
+        Accept: "application/json"
+      },
+      auth: {
+        username: "apikey",
+        password: "XT2uW-VFHweqxjxZ05Wz5PBNGukv7Dg4nPoPBtjWlA1L"
+      }
+    };
+    axios(url, config)
+      .then(onSuccess)
+      .catch(onError);
+  }
+
+  static getAllMessages(username, onSuccess, onError) {
+    const url = "https://localhost:44373/api/atthackathon/" + username;
+    const config = {
+      method: "GET",
+      headers: { "Content-Type": "application/json" }
+    };
+    axios(url, config)
+      .then(onSuccess)
+      .catch(onError);
+  }
+}
+
+export default PersonalityInsightsService;
